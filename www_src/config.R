7fb9ce3bfb0a8855753c5cd219ed50932038245f
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2024-07-18 12:20:04 +0000

    1.4-26
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multcomp@504 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www_src/config.R
+++ www_src/config.R
@@ -3,5 +3,5 @@ pkg <- "multcomp"
 dest <- "html"
 publish <- "../www"
 
-download.file("http://user.math.uzh.ch/hothorn/TH.bib", dest = "TH.bib")
+download.file("http://user.math.uzh.ch/hothorn/bib/TH.bib", dest = "TH.bib")
 bib <- "TH.bib"
